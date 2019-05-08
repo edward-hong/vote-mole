e6f8caf17d57f3aeb15b44f63f4b35131859429a
Edward Hong <edward.hong527@gmail.com>
2019-05-08 22:16:11 +1200

    chore: add hmr

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,11 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if (process.env.NODE_ENV !== 'production') {
+	if (module.hot) {
+		module.hot.accept('./components/App', () => {
+			ReactDOM.render(<App />, document.querySelector('#root'))
+		})
+	}
+}
