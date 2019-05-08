1689b5af7f9dd6573dce35d9faaedf7488a985ce
Edward Hong <edward.hong527@gmail.com>
2019-05-09 11:38:43 +1200

    chore: add hmr to redux-observables

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { BehaviorSubject } from 'rxjs'
+import { switchMap } from 'rxjs/operators'
 import { createEpicMiddleware } from 'redux-observable'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { not, equals, and, isNil } from 'ramda'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './components/App'
@@ -17,14 +20,18 @@ const epicMiddleware = createEpicMiddleware()
 
 const appliedMiddleware = applyMiddleware(epicMiddleware)
 
-const middleware =
-	NODE_ENV !== 'production'
-		? composeWithDevTools(appliedMiddleware)
-		: compose(appliedMiddleware)
+const middleware = not(equals(NODE_ENV, 'production'))
+	? composeWithDevTools(appliedMiddleware)
+	: compose(appliedMiddleware)
 
 const store = createStore(rootReducer, initialState, middleware)
 
-epicMiddleware.run(rootEpic)
+const epic$ = new BehaviorSubject(rootEpic)
+
+const hotReloadingEpic = (...args) =>
+	epic$.pipe(switchMap(epic => epic(...args)))
+
+epicMiddleware.run(hotReloadingEpic)
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -33,8 +40,12 @@ ReactDOM.render(
 	document.getElementById('root')
 )
 
-if (NODE_ENV !== 'production') {
+if (and(not(equals(NODE_ENV, 'production')), not(isNil(module.hot)))) {
 	if (module.hot) {
+		module.hot.accept('./state/epics', () => {
+			const nextRootEpic = require('./state/epics').rootEpic
+			epic$.next(nextRootEpic)
+		})
 		module.hot.accept('./components/App', () => {
 			ReactDOM.render(<App />, document.querySelector('#root'))
 		})
