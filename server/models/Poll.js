e16e9aac74fecc0ab40ca54a7e4ee84dd4182c7c
Edward Hong <edward.hong527@gmail.com>
2019-05-09 11:38:52 +1200

    feature: add Poll model

---
+++ server/models/Poll.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const pollSchema = new Schema({
+	userID: String,
+	pollQuestion: String,
+	pollOptions: [
+		{
+			name: String,
+			quantity: Number,
+		},
+	],
+	IP: { type: [String], default: [] },
+})
+
+const Polls = mongoose.model('poll', pollSchema)
+
+module.exports = Polls
