7f9b82d72f719128d4d51ce02e8243e4edbda3ba
Edward Hong <edward.hong527@gmail.com>
2019-05-24 09:18:46 +1200

    feature: add redis caching

--- server/routes/pollRoutes.js
+++ server/routes/pollRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { equals, not, isNil, gt, map } = require('ramda')
+const redis = require('promise-redis')()
+const { equals, not, isNil, gt, map, is, pipe } = require('ramda')
 
 const requireLogin = require('../middlewares/requireLogin')
 const Poll = require('../models/Poll')
 
 const poll = express.Router()
+
+// Redis configuration
+const redisClient = redis.createClient(process.env.REDIS_URL)
+const redisExpiryTime = 60 // in seconds
 
 // Determine find query from route
 const determineFindQuery = req =>
@@ -21,23 +26,77 @@ const getLimit = req => {
 	return gt(limit, 50) ? 50 : limit
 }
 
+// Handle stringifying and parsing polls data
+
+const stringifyPolls = pipe(
+	map(JSON.stringify),
+	JSON.stringify
+)
+
+const parsePolls = pipe(
+	JSON.parse,
+	map(JSON.parse)
+)
+
 // Controller used for both to get all polls and get user's polls
 const findData = (req, res) => {
 	const offset = getOffset(req)
 	const limit = getLimit(req)
 
-	const countPromise = determineFindQuery(req).countDocuments()
-	const pollsPromise = determineFindQuery(req)
-		.sort({ _id: -1 })
-		.skip(offset)
-		.limit(limit)
-		.then(polls =>
-			map(({ _id, pollQuestion }) => ({ id: _id, pollQuestion }), polls)
-		)
-
-	Promise.all([countPromise, pollsPromise]).then(([count, polls]) => {
-		res.json({ count, polls })
+	// Generate unique key for redis caching
+	const redisCacheKey = JSON.stringify({
+		...determineFindQuery(req)
+			.sort({ _id: -1 })
+			.skip(offset)
+			.limit(limit)
+			.getOptions(),
+		path: req.path,
 	})
+
+	// Get cached data from redis
+	const getCountFromRedis = redisClient.hget(redisCacheKey, 'polls')
+	const getPollsFromRedis = redisClient.hget(redisCacheKey, 'count')
+
+	Promise.all([getCountFromRedis, getPollsFromRedis])
+		.then(([polls, count]) => {
+			if (is(String, polls)) {
+				res.json({
+					count,
+					polls: parsePolls(polls),
+				})
+			} else {
+				return Promise.reject()
+			}
+		})
+		.catch(() => {
+			const countPromise = determineFindQuery(req).countDocuments()
+			const pollsPromise = determineFindQuery(req)
+				.sort({ _id: -1 })
+				.skip(offset)
+				.limit(limit)
+				.then(polls =>
+					map(({ _id, pollQuestion }) => ({ id: _id, pollQuestion }), polls)
+				)
+
+			Promise.all([countPromise, pollsPromise]).then(([count, polls]) => {
+				// Heroku's free tier doesn't provide data persistence for Redis.
+				// So only pass in expiry arguments when app isn't in production
+
+				const redisCountArr = [redisCacheKey, 'count', count]
+				const redisPollsArr = [redisCacheKey, 'polls', stringifyPolls(polls)]
+
+				if (process.env.NODE_ENV !== 'production') {
+					const expireArr = ['EX', redisExpiryTime]
+					redisClient.hset(...redisCountArr, ...expireArr)
+					redisClient.hset(...redisPollsArr, ...expireArr)
+				} else {
+					redisClient.hset(...redisCountArr)
+					redisClient.hset(...redisPollsArr)
+				}
+
+				res.json({ count, polls })
+			})
+		})
 }
 
 // Submit new poll
@@ -50,6 +109,7 @@ poll.post('/submit', requireLogin, (req, res) => {
 		if (foundPoll) {
 			res.status(406).send('failed')
 		} else {
+			redisClient.flushall()
 			new Poll(req.body).save().then(savedPoll => res.send(savedPoll._id))
 		}
 	})
@@ -118,6 +178,7 @@ poll.put('/vote', (req, res) => {
 
 // Delete poll
 poll.delete('/delete/:id', requireLogin, (req, res) => {
+	redisClient.flushall()
 	Poll.findByIdAndRemove(req.params.id).then(deletedPoll => {
 		if (deletedPoll) res.send({ status: 'Poll deleted' })
 		else res.status(400).send({ status: 'Poll not found' })
