ce8734b8891702fde9eaf82ca9d50c2cfd328a02
Edward Hong <edward.hong527@gmail.com>
2019-05-24 00:10:43 +1200

    feature: add backend auth for facebook, twitter and github

--- server/auth/setupPassport.js
+++ server/auth/setupPassport.js
@@ -2,11 +2,23 @@ const path = require('path')
 const passport = require('passport')
 const mongoose = require('mongoose')
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20')
+const { Strategy: FacebookStrategy } = require('passport-facebook')
+const { Strategy: TwitterStrategy } = require('passport-twitter')
+const { Strategy: GithubStrategy } = require('passport-github')
 
 // Env variables
 require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '.env') })
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
+const {
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET,
+	FACEBOOK_CLIENT_ID,
+	FACEBOOK_SECRET_KEY,
+	GITHUB_CLIENT_ID,
+	GITHUB_SECRET_KEY,
+	TWITTER_CLIENT_ID,
+	TWITTER_SECRET_KEY,
+} = process.env
 
 // Get User model
 const User = mongoose.model('user')
@@ -45,6 +57,49 @@ passport.use(
 			clientID: GOOGLE_CLIENT_ID,
 			clientSecret: GOOGLE_CLIENT_SECRET,
 			callbackURL: '/api/auth/google/callback',
+			proxy: true,
+		},
+		login
+	)
+)
+
+// Facebook Strategy
+
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: FACEBOOK_CLIENT_ID,
+			clientSecret: FACEBOOK_SECRET_KEY,
+			callbackURL: '/api/auth/facebook/callback',
+			profileFields: ['id', 'name'],
+			proxy: true,
+		},
+		login
+	)
+)
+
+// Twitter Strategy
+
+passport.use(
+	new TwitterStrategy(
+		{
+			consumerKey: TWITTER_CLIENT_ID,
+			consumerSecret: TWITTER_SECRET_KEY,
+			callbackURL: '/api/auth/twitter/callback',
+			proxy: true,
+		},
+		login
+	)
+)
+
+// Github Strategy
+
+passport.use(
+	new GithubStrategy(
+		{
+			clientID: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_SECRET_KEY,
+			callbackURL: '/api/auth/github/callback',
 			proxy: true,
 		},
 		login
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -15,6 +15,34 @@ auth.get('/google/callback', passport.authenticate('google'), (req, res) => {
 	res.redirect('/')
 })
 
+// Facebook OAuth routes
+
+auth.get('/facebook', passport.authenticate('facebook'))
+
+auth.get(
+	'/facebook/callback',
+	passport.authenticate('facebook'),
+	(req, res) => {
+		res.redirect('/')
+	}
+)
+
+// Twitter OAuth routes
+
+auth.get('/twitter', passport.authenticate('twitter'))
+
+auth.get('/twitter/callback', passport.authenticate('twitter'), (req, res) => {
+	res.redirect('/')
+})
+
+// Github OAuth routes
+
+auth.get('/github', passport.authenticate('github'))
+
+auth.get('/github/callback', passport.authenticate('github'), (req, res) => {
+	res.redirect('/')
+})
+
 // Check if logged in
 auth.get('/current_user', (req, res) => {
 	res.send(req.user)
