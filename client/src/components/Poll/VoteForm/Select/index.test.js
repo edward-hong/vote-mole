bac284f70b6d574d308b23349f4818e82781f3f1
Edward Hong <edward.hong527@gmail.com>
2019-06-26 21:30:02 +1200

    test: Select

---
+++ client/src/components/Poll/VoteForm/Select/index.test.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Select from '.'
+
+const pollOptions = [
+	{
+		votes: 3,
+		option: 'English',
+	},
+	{
+		votes: 6,
+		option: 'French',
+	},
+	{
+		votes: 9,
+		option: 'German',
+	},
+	{
+		votes: 5,
+		option: 'Russian',
+	},
+]
+
+describe('Select', () => {
+	it('shows error message when there is an error', () => {
+		const error = 'Please make a selection'
+		const { getByText } = render(
+			<Select
+				input={{ name: 'select' }}
+				meta={{ touched: true, error }}
+				pollOptions={pollOptions}
+				type="select"
+			/>,
+		)
+
+		expect(getByText(error)).toHaveTextContent(error)
+	})
+
+	it('renders correctly when valid', () => {
+		const { container } = render(
+			<Select
+				input={{ name: 'select' }}
+				meta={{ touched: true, error: false }}
+				pollOptions={pollOptions}
+				type="select"
+			/>,
+		)
+
+		expect(container.firstChild).toMatchSnapshot()
+	})
+
+	it('renders correctly when untouched', () => {
+		const { container } = render(
+			<Select
+				input={{ name: 'select' }}
+				meta={{ touched: false, error: false }}
+				pollOptions={pollOptions}
+				type="select"
+			/>,
+		)
+
+		expect(container.firstChild).toMatchSnapshot()
+	})
+})
